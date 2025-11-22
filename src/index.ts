0f0f998a179750e445bf5ae5e004470886d2d0bf
ManuelB <juanman234@gmail.com>
2025-11-22 13:32:38 +1100

    port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8888;
 
 // Middleware
 app.use(express.json());
